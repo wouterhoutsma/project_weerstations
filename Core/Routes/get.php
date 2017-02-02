a9a5f31f0b497eb3270691acaf94f35688349923
Stijn Koster <h.s.koster@st.hanze.nl>
2017-02-02 11:43:13 +0100

    Create new account page

--- Core/Routes/get.php
+++ Core/Routes/get.php
@@ -4,6 +4,6 @@
   //echo "Test";
   Router::route('/settings/{user_id}', 'UserController.show_settings'); // User panel
   Router::route('/', 'FrontpageController.index');  // Frontpage
-  Router::route('/stijn', 'StijnTest.index'); # Stijn view test
+  Router::route('/createNewAccount', 'StijnTest.index'); # Stijn view test
   Router::route('/assets/{file_name}', 'AssetController.asset');
 ?>
