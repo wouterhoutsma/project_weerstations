f94811040a60b70579121df093a1496f7843e0b2
Talitha <t.g.i.voortman@st.hanze.nl>
2017-02-07 16:55:20 +0100

    Made the first time logging in page

--- Controllers/FrontpageController.php
+++ Controllers/FrontpageController.php
@@ -39,15 +39,100 @@
           return;
       }
 
-      $auth = Auth::getInstance();
-      $tijd = time();
-      $hash = Auth::calculateHash($result->user_id, $tijd);
-      $auth->insert([
-          'hash' => $hash,
-          'user_id' => $result->user_id,
-          'timestamp' => $tijd
-      ]);
-      $_SESSION['user'] = $hash;
+      if(!isset($result->firstname)
+          || !isset($result->lastname)
+          || !isset($result->phone)){
+            $email = $result->email;
+            $title = "First login";
+            $oldpassword = $result->password;
+            $this->view('firstlogin', compact('title', 'email', 'oldpassword'));
+            return;
+      }
+
+
+      $this->make_session($result->user_id);
+      header("Location: /");  # Redirect to host (weather.app ie)
+    }
+    private function make_session($user_id){
+        $auth = Auth::getInstance();
+        $tijd = time();
+        $hash = Auth::calculateHash($user_id, $tijd);
+        $auth->insert([
+            'hash' => $hash,
+            'user_id' => $user_id,
+            'timestamp' => $tijd
+        ]);
+        $_SESSION['user'] = $hash;
+    }
+
+    public function firstlogin(){
+      $title = "First login";
+      $error = "";
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $phonenumber = $_POST['phonenumber'];
+        $email = $_POST['emailaddress'];
+        $oldemail = $_POST['oldemail'];
+        $oldpassword = $_POST['oldpassword'];
+      if($_POST['firstname'] == ""
+          || $firstname == ""
+          || $lastname == ""
+          || $phonenumber == ""
+          || $email == ""
+          || $_POST['newpassword'] == ""
+          || $_POST['confirmpassword'] == "") {
+        $error .= "<li>All fields have to be filled in, except for the file</li>";
+      }
+
+      if($_POST['newpassword'] != $_POST['confirmpassword']){
+        $error .= "<li>The passwords don't match</li>";
+      }
+      elseif(preg_match('/[A-Z]+[a-z]+[0-9]+/', $_POST['newpassword'])){
+
+      }
+
+
+
+      $firstname = ucfirst(strtolower($firstname));
+      $lastname = ucfirst(strtolower($lastname));
+      $acceptnumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+      $arrayphone = str_split($phonenumber);
+      foreach($arrayphone as $value) {
+        if(in_array($value, $acceptnumbers)) {
+            continue;
+          }
+          else {
+            $error .= "<li>Only fill numbers in the phonenumber</li>";
+          }
+
+      }
+      $user = new User();
+      $user_id = $user->select(['user_id'])
+          ->where('email', '=', $oldemail, 1)
+          ->where('password', '=', $oldpassword, 1)
+          ->first();
+      if(!$user_id)
+        $error .= "<li>Don't mess with the HTML please. Try going back and logging in again.</li>";
+
+      if($error != ""){
+          $error = sprintf("<ul>%s</ul>", $error);
+          $this->view('firstlogin', compact('title', 'error', 'firstname', 'lastname', 'phonenumber', 'email', 'oldemail', 'oldpassword'));
+          return;
+      }
+      //$this->view('firstlogin', compact('title', 'firstname', 'lastname', 'phonenumber', 'email', 'oldemail', 'oldpassword'));
+      //return;
+
+
+      $password = User::make_password($_POST['newpassword']);
+      $user->update([
+          'firstname' => $firstname,
+          'lastname' => $lastname,
+          'phone' => $phonenumber,
+          'email' => $email,
+          'password' => $password
+      ])->where('user_id', '=', $user_id->user_id)->execute();
+
+      $this->make_session($user_id->user_id);
       header("Location: /");  # Redirect to host (weather.app ie)
     }
   }
